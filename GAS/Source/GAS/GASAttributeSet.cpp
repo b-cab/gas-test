54c2c45d2e7552a97b5e61d66b8bacd8a340809b
Bien Cabungcal <bien.cabungcal@touchcast.com>
2024-01-08 21:26:34 +0800

    Add GAS Test Implementation

---
+++ GAS/Source/GAS/GASAttributeSet.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GASAttributeSet.h"
+#include "Net/UnrealNetwork.h"
+
+UGASAttributeSet::UGASAttributeSet()
+{
+}
+
+void UGASAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASAttributeSet, FireWeapon, COND_None, REPNOTIFY_Always);
+}
+
+void UGASAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASAttributeSet, Health, OldHealth);
+}
+
+void UGASAttributeSet::OnRep_FireWeapon(const FGameplayAttributeData& OldFireWeapon)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASAttributeSet, FireWeapon, OldFireWeapon);
+}
